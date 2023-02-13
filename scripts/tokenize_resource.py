90be690809cefdc64fb5c09d9e37cfef2c9d1d06
Mycroft <thkang0629@gmail.com>
2023-02-13 15:08:44 +0900

    Add encoding

--- scripts/tokenize_resource.py
+++ scripts/tokenize_resource.py
@@ -136,7 +136,7 @@ def main(
         else:
             texts = (line["text"] for line in dataset)
 
-    with open(output_file, "w") as output_fileh, Pool(processes=n_process) as pool:
+    with open(output_file, "w", encoding="utf-8") as output_fileh, Pool(processes=n_process) as pool:
         result = pool.imap(partial(tokenize_batch, nlp), chunked(texts, batch_size))
         for lines in result:
             output_fileh.writelines(lines)
